d5c965499d30d850df5be89667f1aa7f58814e04
Lunpe <gautier.claisse@gmail.com>
2015-05-11 04:05:37 +0200

    Added run.py

---
+++ run.py
@@ -0,0 +1,12 @@
+import neuralnet
+import layers
+import nnutils
+
+# Fully connected network learning the mnist database
+print("Test: a fully connected network on the mnist database.")
+print("Loading the mnist database...")
+tr_d, te_d = nnutils.load_final_data()
+net = neuralnet.SoftmaxFCNetwork(tr_d[0][0].shape, [100, 10], 10, 50, 0.01, 0)
+net.train(tr_d[0], tr_d[1])
+res = net.test(te_d[0], te_d[1])
+print(str(res) + ' / ' + str(len(te_d[0])))

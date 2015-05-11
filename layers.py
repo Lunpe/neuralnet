da3e900d9156eedfb6178a1204ee04fdb0c6f5ba
Lunpe <gautier.claisse@gmail.com>
2015-05-11 03:57:49 +0200

    FC network now working with the Net/Layer model

--- layers.py
+++ layers.py
@@ -1,22 +1,23 @@
 import numpy as np
 
-class Layer():
+class Layer(object):
 	""" Abstract class of a layer. """
 
-	def __init__(self, shape):
-		self.width = shape[0]
-		self.height = shape[1]
-		self.depth = 1 if len(shape) <= 2 else shape[3]
+	def __init__(self, input_shape):
+		self.input_shape = input_shape
+		self.output_shape = input_shape # Most layers don't change the shape
+		self.acts = None
+		self.gradient = None
 
 	def forward(self, inputs, keepacts=False):
-		assert inputs.shape == self.input_shape, 'Wrong input shape'
-		self._forward(inputs, keepacts)
+		assert inputs[0].shape == self.input_shape, 'Wrong input shape'
+		return self._forward(inputs, keepacts)
 
 	def backward(self, gradient, keepgrad=True):
-		assert gradient.shape == self.output_shape, 'Wrong gradient shape'
-		self._backward(gradient, keepgrad)
+		assert self.acts != [], "No activation values stored, backprop not possible"
+		return self._backward(gradient, keepgrad)
 
-	def update_parameters():
+	def update_parameters(self, learn_rate, regu_strength):
 		""" To be implemented in child class (if needed). """
 		pass
 
@@ -30,6 +31,7 @@ class Layer():
 
 
 class ConvLayer(Layer):
+	# TODO: Implement the ConvLayer
 
 	def __init__(self):
 		# Hyperparameters
@@ -47,11 +49,12 @@ class ConvLayer(Layer):
 	def _backward(self, gradient, keepgrad=True):
 		pass
 
-	def update_parameters():
+	def update_parameters(self):
 		pass
 
 
 class PoolLayer(Layer):
+	# TODO: Implement the PoolLayer
 
 	def __init__(self):
 		pass
@@ -66,46 +69,65 @@ class PoolLayer(Layer):
 		pass
 
 
-class ReluLayer(Layer):
-
-	def __init__(self):
-		pass
+class ReLuLayer(Layer):
 
 	def _forward(self, inputs, keepacts=False):
-		pass
+		acts = np.maximum(0, inputs)
+		if keepacts:
+			self.acts = acts
+		return acts
 
 	def _backward(self, gradient, keepgrad=True):
-		pass
+		grad = gradient * (self.acts > 0)
+		if keepgrad:
+			self.gradient = grad
+		return grad
 
-	def update_parameters():
-		pass
 
-class FCLayer():
+class BiasLayer(Layer):
 
-	def __init__(self):
-		pass
+	def __init__(self, input_shape):
+		super(BiasLayer, self).__init__(input_shape)
+		self.biases = np.zeros(input_shape)
 
 	def _forward(self, inputs, keepacts=False):
-		pass
+		acts = inputs + self.biases
+		if keepacts:
+			self.acts = acts
+		return acts
 
 	def _backward(self, gradient, keepgrad=True):
-		pass
+		if keepgrad:
+			self.gradient = gradient
+		return gradient
 
-	def update_parameters():
-		pass
+	def update_parameters(self, learn_rate, regu_strength):
+		self.biases -= learn_rate * np.mean(self.gradient, axis=0)
 
 
-class SoftmaxLayer(Layer):
+class FCLayer(Layer):
 
-	def __init__(self):
-		pass
+	def __init__(self, input_shape, n_neurons):
+		super(FCLayer, self).__init__(input_shape)
+		n_input = np.prod(input_shape)
+		self.weights = np.random.randn(n_input, n_neurons)
+		self.weights *= np.sqrt(2.0 / n_input)
+		self.output_shape = (n_neurons,)
 
 	def _forward(self, inputs, keepacts=False):
-		pass
+		self.inputs = inputs
+		acts = np.dot(inputs, self.weights)
+		if keepacts:
+			self.acts = acts
+		return acts
 
 	def _backward(self, gradient, keepgrad=True):
-		pass
+		grad = np.dot(self.inputs.T, gradient)
+		if keepgrad:
+			self.gradient = grad
+		return np.dot(gradient, self.weights.T)
 
-	def update_parameters():
-		pass
+	def update_parameters(self, learn_rate, regu_strength):
+		self.weights *= (1 - regu_strength)
+		self.weights -= self.gradient * learn_rate / len(self.gradient)
 
--- neuralnet.py
+++ neuralnet.py
@@ -1,24 +1,24 @@
-import nnutils
 import layers
 import numpy as np
 import itertools as it
 
-class NeuralNetwork():
+class NeuralNetwork(object):
 	""" Abstract class of a neural network.
 		To use a neuralnet one must create a child class (of NeuralNetwork
 		or one of its subclasses) that instantiates its own layers. """
 
-	def __init__(self, n_epochs, batch_size, learn_rate):
+	def __init__(self, input_shape, n_epochs, batch_size, learn_rate, regu_strength):
+		self.input_shape = input_shape
 		self.layers = []
 		self.n_epochs = n_epochs
 		self.batch_size = batch_size
 		self.learn_rate = learn_rate
+		self.regu_strength = regu_strength
 
 	def forward(self, x, keepacts=False):
-		activs = data[0]
 		for layer in self.layers:
-			activs = layer.forward(activs, keepacts=keepacts)
-		return activs
+			x = layer.forward(x, keepacts=keepacts)
+		return x
 
 	def backward(self, gradient, keepgrad=True):
 		for layer in reversed(self.layers):
@@ -26,28 +26,28 @@ class NeuralNetwork():
 		return gradient
 
 	def test(self, x, y):
-		maxs = np.argmax(self.forward(x), axis=0)
+		maxs = np.argmax(self.forward(x), axis=1)
 		return sum((o == y) for (o, y) in it.izip(maxs, y))
 
 	def train(self, x, y):
 		""" May be reimplemented in a child class. """
 		for epoch in xrange(self.n_epochs):
+			print('epoch ' + str(epoch + 1) + ' / ' + str(self.n_epochs))
 			for i_batch in xrange(0, len(x), self.batch_size):
 				xb = x[i_batch:i_batch+self.batch_size]
 				yb = y[i_batch:i_batch+self.batch_size]
 
 				outputs = self.forward(xb, keepacts=True)
-
-				# Computing the loss gradient
-				losses = self.loss(outputs, yb)
-				y_mat = np.zeros(losses.shape)
-				for i, i_y in enumerate(yb):
-					y_mat[yb[i]] = 1
-				grad = losses - y_mat
-
+				grad = self.loss(outputs, yb)
 				self.backward(grad)
+				self.update_parameters()
 
-				update_parameters()
+	def _add_layer(self, layer_class, **kwargs):
+		if len(self.layers) == 0:
+			self.layers.append(layer_class(self.input_shape, **kwargs))
+		else:
+			self.layers.append(layer_class(self.layers[-1].output_shape, **kwargs))
+		return self
 
 	def update_parameters(self):
 		""" To be implemented in child class. """
@@ -63,15 +63,37 @@ class ConvNet(NeuralNetwork):
 	def __init__(self, n_epochs, batch_size, learn_rate):
 		super(ConvNet, self).__init__(n_epochs, batch_size, learn_rate)
 		self.layers = []
-		layers.append(layers.ConvLayer())
-		layers.append(layers.ReluLayer())
-		layers.append(layers.ConvLayer())
-		layers.append(layers.ReluLayer())
-		layers.append(layers.PoolLayer())
-		layers.append(layers.FCLayer())
-		layers.append(layers.SoftmaxLayer())
+		self._add_layer(layers.ConvLayer)
+		self._add_layer(layers.ReluLayer)
+		self._add_layer(layers.ConvLayer)
+		self._add_layer(layers.ReluLayer)
+		self._add_layer(layers.PoolLayer)
+		self._add_layer(layers.FCLayer)
 
 	def update_parameters():
 		pass
 
+
+class SoftmaxFCNetwork(NeuralNetwork):
+
+	def __init__(self, input_shape, layout, n_epochs, batch_size, learn_rate, regu_strength):
+		super(SoftmaxFCNetwork, self).__init__(input_shape, n_epochs, batch_size, learn_rate, regu_strength)
+		for n_neurons in layout[:-1]:
+			self._add_layer(layers.FCLayer, n_neurons=n_neurons)
+			self._add_layer(layers.BiasLayer)
+			self._add_layer(layers.ReLuLayer)
+		# There's no activation function on the last layer
+		self._add_layer(layers.FCLayer, n_neurons=layout[-1])
+		self._add_layer(layers.BiasLayer)
+
+	def update_parameters(self):
+		for layer in self.layers:
+			layer.update_parameters(self.learn_rate, self.regu_strength)
+
+	def loss(self, outputs, ys):
+		# Softmax function
+		grad = np.array([np.exp(o) / np.sum(np.exp(o)) for o in outputs])
+		for i, c in enumerate(ys):
+			grad[i][c] -= 1
+		return grad
 

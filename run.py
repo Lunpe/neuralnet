8d587b08631add3c8d1d7d60ecc6dbe434a09852
Lunpe <gautier.claisse@gmail.com>
2015-05-11 12:51:26 +0200

    Renamed neuralnets.py

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import neuralnet
+import neuralnets
 import layers
 import nnutils
 
@@ -6,7 +6,7 @@ import nnutils
 print("Test: a fully connected network on the mnist database.")
 print("Loading the mnist database...")
 tr_d, te_d = nnutils.load_final_data()
-net = neuralnet.SoftmaxFCNetwork(tr_d[0][0].shape, [100, 10], 10, 50, 0.01, 0)
+net = neuralnets.SoftmaxFCNetwork(tr_d[0][0].shape, [100, 10], 10, 50, 0.01, 0)
 net.train(tr_d[0], tr_d[1])
 res = net.test(te_d[0], te_d[1])
 print(str(res) + ' / ' + str(len(te_d[0])))
